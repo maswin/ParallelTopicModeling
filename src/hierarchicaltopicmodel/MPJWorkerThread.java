23e274a2d7b373064d79a9baf140872ed1407b71
sidharthmanohar <sidharthmanohar157@gmail.com>
2015-06-24 22:24:11 +0530

    Cleaned Unwanted code

--- src/hierarchicaltopicmodel/MPJWorkerThread.java
+++ src/hierarchicaltopicmodel/MPJWorkerThread.java
@@ -16,80 +16,75 @@ import cc.mallet.util.Randoms;
 public class MPJWorkerThread {
 
 	String files[] = null;
-	int id; int size;
+	int id;
+	int size;
 	String[] inputParameters;
-	ConcurrentHashMap<Integer,ArrayList<NCRPNode>> rootMap;// = new ConcurrentHashMap<Integer,ArrayList<NCRPNode>>();
+	ConcurrentHashMap<Integer, ArrayList<NCRPNode>> rootMap;// = new
+															// ConcurrentHashMap<Integer,ArrayList<NCRPNode>>();
 	ArrayList<NCRPNode> leaves;// = new ArrayList<NCRPNode>();
 
-	public MPJWorkerThread(String[] files, int id, int size, String[] inputParameters){
-		this.files = files.clone();	
+	public MPJWorkerThread(String[] files, int id, int size,
+			String[] inputParameters) {
+		this.files = files.clone();
 		this.id = id;
 		this.size = size;
 		this.inputParameters = inputParameters.clone();
 	}
 
-	public ArrayList<NCRPNode> formTrees(){
-		System.out.println("THREAD "+id+" : Starting\n");
-		
+	public ArrayList<NCRPNode> formTrees() {
+		System.out.println("THREAD " + id + " : Starting\n");
+
 		File inputDirectory = getInputFiles();
 		removeStopWords(inputDirectory);
-		
+
 		HierarchicalLDA hlda = constructTopicHierarchy(inputDirectory);
-		SubTreeExtractor se = new SubTreeExtractor();
-		
+
 		ArrayList<NCRPNode> rootList = new ArrayList<NCRPNode>();
-		
-		if(id==0){
-				
-			rootList.add(hlda.getRootNode());
 
-		}else{
-			
-			se.extractRoot(hlda.getRootNode());
-			int size = se.getRootSize(); 
-			for(int j = 0; j < size; j++)
-				rootList.add(se.getRoot(j));
+		rootList.add(hlda.getRootNode());
 
-		}
-		System.out.println("THREAD "+id+" : Terminating\n");
+		System.out.println("THREAD " + id + " : Terminating\n");
 		return rootList;
 	}
-	public File getInputFiles(){
 
-		//String dirName = "C:\\mallet-2.0.7\\mallet-2.0.7\\Directory"+(threadID+1);
-		String dirName = "var/Directory"+(id+1);
+	public File getInputFiles() {
+
+		// String dirName =
+		// "C:\\mallet-2.0.7\\mallet-2.0.7\\Directory"+(threadID+1);
+		String dirName = "var/Directory" + (id + 1);
 		File dir = new File(dirName);
 		dir.mkdir();
 
 		int noOfFilesToProcess = (files.length) / (size);
-		//int noOfFilesToProcess = (files.length) / (10);
-		int startIndex = (id*noOfFilesToProcess);
-		int endIndex = (id*noOfFilesToProcess)+noOfFilesToProcess;
+		// int noOfFilesToProcess = (files.length) / (10);
+		int startIndex = (id * noOfFilesToProcess);
+		int endIndex = (id * noOfFilesToProcess) + noOfFilesToProcess;
 
-		//int startIndex = 0;
-		//int endIndex = files.length;
+		// int startIndex = 0;
+		// int endIndex = files.length;
 
-		for(int i=startIndex;i<endIndex;i++) {
+		for (int i = startIndex; i < endIndex; i++) {
 
 			File source = new File(files[i]);
-			File target = new File(dirName+"/"+source.getName());
+			File target = new File(dirName + "/" + source.getName());
 
 			try {
-				FileUtils.copyFile(source, target);			
+				FileUtils.copyFile(source, target);
 			}
 
-			catch(IOException e) {
+			catch (IOException e) {
 				e.printStackTrace();
 			}
 
 		}
 		return dir;
 	}
+
 	public void removeStopWords(File inputDirectory) {
 
 		String command1 = "cd /home/aswin/mallet-2.0.7/\nbin/mallet import-dir --input " + inputDirectory.getAbsolutePath()+ " --output " + inputDirectory.getAbsolutePath() + ".mallet --keep-sequence --remove-stopwords"; //linux Change
 		String filename1 = "/home/aswin/workspace/ParallelTopicModeling/var/"+inputDirectory.getName()+"Features.sh"; //linux Change
-
+		
 		InputOutputReader ior = new InputOutputReader();
 		ior.processInput(command1, filename1);
 
@@ -97,8 +92,9 @@ public class MPJWorkerThread {
 
 	public HierarchicalLDA constructTopicHierarchy(File inputDirectory) {
 
-		inputParameters[0] = inputParameters[1] = (inputDirectory.getAbsolutePath()+ ".mallet");
-		inputParameters[3] = inputDirectory.getAbsolutePath()+"WC.txt";
+		inputParameters[0] = inputParameters[1] = (inputDirectory
+				.getAbsolutePath() + ".mallet");
+		inputParameters[3] = inputDirectory.getAbsolutePath() + "WC.txt";
 
 		InputOutputReader ior = new InputOutputReader();
 		HierarchicalLDA hlda = ior.setHldaParameters(inputParameters);
@@ -110,18 +106,20 @@ public class MPJWorkerThread {
 		if (inputParameters[1] != null) {
 			testing = InstanceList.load(new File(inputParameters[1]));
 		}
-		Randoms random = new Randoms(); //Initialize Random Number Generator
+		Randoms random = new Randoms(); // Initialize Random Number Generator
 
-		//Gathering input files
+		// Gathering input files
 		String[] fileNames = new String[inputDirectory.listFiles().length];
 		int i = 0;
-		for(File f : inputDirectory.listFiles()) 
+		for (File f : inputDirectory.listFiles())
 			fileNames[i++] = f.getAbsolutePath();
 
 		// Initialize and start the sampler
-		System.out.println("File Size : "+instances.size()+" "+file);
-		hlda.initialize(instances, testing , Integer.parseInt(inputParameters[7]), random,fileNames);
-		hlda.estimate(Integer.parseInt(inputParameters[4]),inputDirectory.getAbsolutePath()+"Tree.txt",fileNames);
+		System.out.println("File Size : " + instances.size() + " " + file);
+		hlda.initialize(instances, testing,
+				Integer.parseInt(inputParameters[7]), random, fileNames);
+		hlda.estimate(Integer.parseInt(inputParameters[4]),
+				inputDirectory.getAbsolutePath() + "Tree.txt", fileNames);
 		hlda.writeWordCounts();
 
 		return hlda;
